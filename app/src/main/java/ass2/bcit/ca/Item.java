441282b87daacf87c89cc9f40e204b2057223db3
sylvartore <sylvartore@gmail.com>
2019-04-10 11:40:39 -0700

    Add icon

--- app/src/main/java/ass2/bcit/ca/Item.java
+++ app/src/main/java/ass2/bcit/ca/Item.java
@@ -6,6 +6,38 @@ public class Item {
     String date_taken;
     String author;
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setMedia(String media) {
+        this.media = media;
+    }
+
+    public void setDate_taken(String date_taken) {
+        this.date_taken = date_taken;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+
+    public String getDate_taken() {
+        return date_taken;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
     @Override
     public String toString() {
         return "Item{" +
